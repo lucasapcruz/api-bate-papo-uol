40c06a7ab57cb0e91d07ceef3628063f0df60ee0
Lucas Cruz <lucas.apcruz@gmail.com>
2022-11-12 03:39:05 -0300

    feat: participants endpoint and body validation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import joi from "joi"
 
 dotenv.config();
 
@@ -10,6 +11,22 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 app.use(cors());
 app.use(express.json());
+
+const participantSchema = joi.object({
+    name: joi.string().required()
+})
+
+app.post("/participants", (req, res) => {
+    const participant = req.body
+    const participantValidation = participantSchema.validate(participant)
+
+    if(participantValidation.error){
+        res.sendStatus(422)
+        return
+    }
+
+    res.sendStatus(201)
+  });
 
 
 app.listen(process.env.SERVER_PORT, () => console.log(`Server running on port ${process.env.SERVER_PORT}`)) ;
